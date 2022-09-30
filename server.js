2a86151b6e7a2b0346604a4e2f81e32b12f019a6
Joon Park <jsp220@gmail.com>
2022-09-30 15:10:51 -0700

    Create helper functions in fsUtil.js, Add get and post functionality for /api/notes

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const { clog } = require('./middleware/clog');
+const { v4: uuidv4 } = require('uuid');
+const {readFromFile, readAndAppend} = require("./helpers/fsUtil");
+
 const PORT = process.env.PORT  || 3001;
 
 const app = express();
@@ -16,14 +19,30 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+app.get('/api/notes', (req, res) => {
+    readFromFile('./db/db.json').then(data => res.json(JSON.parse(data)));
+})
+
+app.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuidv4()
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json('Note added successfully.')
+    } else {
+        res.error("Error");
+    }    
+});
+
 app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
-
-// app.get('/api/notes', (req, res) => {
-
-// })
-
 
 app.listen(PORT, () =>
     console.log(`App listening at ${PORT}`)
